dcbd3d41922b9f1845f9a598d96866bc5664f972
divinjordan <nguekeu3divin@gmail.com>
2025-08-03 22:23:12 +0100

    Update get projects list';

---
+++ app/Http/Controllers/Project/GetLastProjects.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Project;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+use App\Models\User;
+
+class GetLastProjects extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        $query = Project::with(['location.division', 'location.country']);
+
+        if ($request->has('user_id')) {
+            $user = User::find($request->user_id);
+
+            if ($user) {
+                $query->whereIn('association_id', function ($q) use ($user) {
+                    $q->select('association_id')
+                      ->from('association_users') // ✅ your custom table
+                      ->where('user_id', $user->id);
+                });
+            }
+        }
+
+        $projects = $query->orderBy('created_at', 'desc')->get();
+
+        return response()->json([
+            'projects' => $projects
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ Route::post('/auth/login', \App\Http\Controllers\Auth\Login::class);
 Route::post('/auth/check-email', \App\Http\Controllers\Auth\CheckEmail::class);
 
 Route::get('/associations', \App\Http\Controllers\Association\GetAssociations::class);
+Route::get('/projects', \App\Http\Controllers\Project\GetProjects::class);
 
 Route::middleware('auth:sanctum')->group(function () {
 
