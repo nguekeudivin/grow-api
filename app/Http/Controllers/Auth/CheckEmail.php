86a0fa5a016252550d994bca3ef2e2e6b99df15c
divinjordan <nguekeu3divin@gmail.com>
2025-08-03 17:53:47 +0100

    Update login

--- app/Http/Controllers/Auth/CheckEmail.php
+++ app/Http/Controllers/Auth/CheckEmail.php
@@ -4,24 +4,28 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\Hash;
-use App\Models\User;
 
-class Login extends Controller
+class CheckEmail extends Controller
 {
     public function __invoke(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'email' => 'required|string',
-            'password' => 'required|string',
+            'email' => 'required|string|email',
+            // 'password' => 'required|string',
         ]);
 
         if ($validator->fails()) {
-            return response()->json(['message' => 'Invalid credentials'], 401);
+            return response()->json(['errors' => $validator->errors()], 422);
         }
 
         $user = User::where('email', $request->email)->first();
+
+        if ($user == null) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Invalid email address'
+            ]);
+        }
 
         if (! $user || ! Hash::check($request->password, $user->password)) {
             return response()->json(['message' => 'Invalid credentials'], 401);
